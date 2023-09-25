2235f3d8ccfb469367e426a14a2ace6ea0be868f
Israt85 <isratanjuman3@gmail.com>
2023-09-25 17:24:40 +0600

    completed all the cards design perfectly

--- src/Components/Card/Card.jsx
+++ src/Components/Card/Card.jsx
@@ -1,8 +1,14 @@
+
 const Card = ({card}) => {
-    const {Category,Picture,Title } = card;
+    const {Category,Picture,Title,Category_bg, Card_bg } = card;
+     const cardStyle = {
+    backgroundColor:Card_bg ,
+    color :Category_bg
+};
+
     return (
         <div>
-           <div className="my-10 mx-4 relative flex max-w-[15rem] flex-col rounded-xl bg-white bg-clip-border text-gray-700 shadow-md">
+           <div className="my-10 mx-4 relative flex max-w-[15rem] flex-col rounded-xl text-gray-700 shadow-md" style={cardStyle}>
   <div className="relative m-0 overflow-hidden rounded-none bg-transparent bg-clip-border text-gray-700 shadow-none">
     <img
       src={Picture}
@@ -10,10 +16,10 @@ const Card = ({card}) => {
     />
   </div>
   <div className="p-6">
-    <h4 className="block font-sans text-2xl font-semibold leading-snug tracking-normal text-blue-gray-900 antialiased">
+    <h4 className=" w-24 rounded text-center block font-sans text-xl font-semibold border" style={{backgroundColor:Category_bg, color: Card_bg }}>
       {Category}
     </h4>
-    <p className="mt-3 block font-sans text-xl font-normal leading-relaxed text-gray-700 antialiased">
+    <p className="mt-3 block font-sans text-xl font-normal leading-relaxed text-gray-700 antialiased" style={{color: Category_bg }}>
      {Title}
     </p>
   </div>
